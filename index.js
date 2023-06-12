1d4673070853c1f87197d5b1266cc2c514d3a9f0
Saurabh Tripathi <saurabh.umesh.tripathi@gmail.com>
2023-06-12 17:25:15 +0530

    commit 2

---
+++ index.js
@@ -0,0 +1,34 @@
+import {addBadge, swipeVertically, getNewDog } from "./utils.js"
+
+
+
+const acceptBtn = document.getElementById('accept')
+const rejectBtn = document.getElementById('reject')
+
+
+function render() {
+    let dog = getNewDog()
+    document.getElementById('dog-desc').innerHTML = dog.getDogHtml()
+    addBadge(dog)
+}
+
+render()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
